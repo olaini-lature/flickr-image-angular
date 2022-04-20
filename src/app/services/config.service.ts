6f9dad88d57810d8017472cd92a117238128f32f
Olaini Lature <olature@hiapp.id>
2022-04-21 03:19:23 +0700

    update API to heroku API

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export const CONSTANTS = {
-  API_FLICKR_BACKEND: 'http://localhost:3030/', // LOCAL DEV
+    //   API_FLICKR_BACKEND: 'http://localhost:3030/', // LOCAL DEV
+    API_FLICKR_BACKEND: 'https://flickr-image-backend.herokuapp.com/', // ONLINE ON HEROKU
 };
